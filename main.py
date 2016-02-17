e49e9f37ec961e2399900fd530af87f86ffe8d1b
froggame <cavestory116@gmail.com>
2016-02-17 18:13:07 -0500

    Added Title

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 # Created in Python 3.5
+print (' ____                      ')
+print ('|  __|                     ')
+print ('| |__    _  __      ___     ___')
+print ('|  __|  | |/__\    / _ \   / _ \')
+print ('| |     |  /  \/  | / \ | | / \ |')
+print ('| |     | |       | \_/ | | \_/ | ')
+print ('|_|     |_|        \___/   \___ |')
+print ('  CARDS                       | |')
+print ('  By Sean E.             /\   | |  ') 
+print ('                         \ \__/ /')
+print ('                          \____/')
